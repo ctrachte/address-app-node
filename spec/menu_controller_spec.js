1604f256acf0041a2b6e035509d6f348f331cd99
Caleb Trachte <cetrachte@hotmail.com>
2018-06-14 16:10:24 -0500

    refactor specs to conform to DRY principles

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -3,15 +3,19 @@
  describe("MenuController", () => {
    describe("#getContactCount()", () => {
 
+     beforeEach(() => {
+       this.menu = new MenuController();
+     });
 // #2
      it("should return 0 when no contacts are in the book", () => {
-       const menu = new MenuController();
-       expect(menu.getContactCount()).toBe(0);
+       expect(this.menu.getContactCount()).toBe(0);
+
      });
+
      it("should return 1 when there is exactly one contact in the book", () => {
-       const menu = new MenuController();
-       menu.contacts.push("Bob");
-       expect(menu.getContactCount()).toBe(1)
+       this.menu.contacts.push("Bob");
+       expect(this.menu.getContactCount()).toBe(1);
      });
+
    });
  });
