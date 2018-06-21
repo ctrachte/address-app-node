6c5fc9022a0f7dfe8aeafa33b02530abcf76a109
Caleb Trachte <cetrachte@hotmail.com>
2018-06-21 13:14:04 -0500

    add getContacts case to menu options

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -11,6 +11,7 @@ module.exports = class MenuController {
         message: "Please choose from an option below: ",
         choices: [
           "Add new contact",
+          "View all contacts",
           "Exit",
           "Get Date"
         ]
@@ -26,6 +27,9 @@ module.exports = class MenuController {
       switch(response.mainMenuChoice){
         case "Add new contact":
           this.addContact();
+          break;
+        case "View all contacts":
+          this.getContacts();
           break;
         case "Exit":
           this.exit();
